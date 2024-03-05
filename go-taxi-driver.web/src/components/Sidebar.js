b70fbf5d01f74a576e30e3136a03eb8c30444d94
danielkarimi <daniel_karimii@outlook.com>
2024-03-05 19:49:09 +0100

    added icons

--- go-taxi-driver.web/src/components/Sidebar.js
+++ go-taxi-driver.web/src/components/Sidebar.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LogoutButton from './LogoutButton'; 
+import HomeIcon from '@material-ui/icons/Home';
+import PersonIcon from '@material-ui/icons/Person';
+import DescriptionIcon from '@material-ui/icons/Description';
+import CommuteIcon from '@material-ui/icons/Commute';
+import DirectionsCarIcon from '@material-ui/icons/DirectionsCar';
+import ReceiptIcon from '@material-ui/icons/Receipt';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+import ContactSupportIcon from '@material-ui/icons/ContactSupport';
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <Link to="/landing">Home</Link>
-      <Link to="/profile">Profile</Link>
-      <Link to="/document">My Documents</Link>
-      <Link to="/rides">My Rides</Link>
-      <Link to="/vehicles">Vehicles</Link>
-      <Link to="/riderinvoice">Rider Invoices</Link>
-      <Link to="/balance">Balance Reports</Link>
-      <Link to="/tax">Tax Reports</Link>
-      <Link to="/guide">Guides & FAQ</Link>
-      <Link to="/contact">Contact</Link>
+      <Link to="/landing"><HomeIcon /> Home</Link>
+      <Link to="/profile"><PersonIcon /> Profile</Link>
+      <Link to="/document"><DescriptionIcon /> My Documents</Link>
+      <Link to="/rides"><CommuteIcon /> My Rides</Link>
+      <Link to="/vehicles"><DirectionsCarIcon /> Vehicles</Link>
+      <Link to="/riderinvoice"><ReceiptIcon /> Rider Invoices</Link>
+      <Link to="/balance"><AccountBalanceIcon /> Balance Reports</Link>
+      <Link to="/tax"><MonetizationOnIcon /> Tax Reports</Link>
+      <Link to="/guide"><QuestionAnswerIcon /> Guides & FAQ</Link>
+      <Link to="/contact"><ContactSupportIcon /> Contact</Link>
 
       <LogoutButton />
     </div>
