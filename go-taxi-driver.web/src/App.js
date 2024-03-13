c74c1ccd527205a66340196006d3f5abf8fe75ac
Oumarbaarry <115113404+Oumarbaarry@users.noreply.github.com>
2024-03-13 16:13:10 +0100

    added changes to MyDocuments.js

--- go-taxi-driver.web/src/App.js
+++ go-taxi-driver.web/src/App.js
@@ -13,7 +13,6 @@ import RiderInvoices from './pages/RiderInvoices.js';
 import BalanceReports from './pages/BalanceReports.js';
 import TaxReports from './pages/TaxReports.js';
 import GuidesFAQ from './pages/GuidesFAQ.js';
-import Sidebar from  "./components/Sidebar.js";
 import Contact from './/pages/Contact';
 import './App.css'
 import './dist/styles.css'; 
@@ -59,51 +58,3 @@ export default App;
 
 
 
-
-// // src/App.js
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Switch, Link, Routes } from 'react-router-dom';
-// import Login from './pages/Login.js';
-// import Register from './pages/Register';
-// import { AppBar, Toolbar, Button } from '@material-ui/core';
-// import Landing from './pages/LandingPage.js';
-// import Profile from './pages/Profile.js';
-// import MyDocuments from './pages/MyDocuments.js';
-// import MyRides from './pages/MyRides.js';
-// import Vehicles from './pages/Vehicles.js';
-// import RiderInvoices from './pages/RiderInvoices.js';
-// import BalanceReports from './pages/BalanceReports.js';
-// import TaxReports from './pages/TaxReports.js';
-// import GuidesFAQ from './pages/GuidesFAQ.js';
-// import Sidebar from './components/sidebar.js';
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <AppBar position="static">
-//         <Toolbar>
-//           <Button color="inherit" component={Link} to="/login">Login</Button>
-//           <Button color="inherit" component={Link} to="/register">Register</Button>
-//         </Toolbar>
-//       </AppBar>
-//       <Routes>
-//         <Route path="/login" element={<Login/>} />
-//         <Route path="/register" element= {<Register/>} />
-//         <Route path="/" element={() => <div>Home</div>} />
-//         <Route path="/landing" element={<Landing/>} />
-//         <Route path="/profile" element={<Profile/>} />
-//         <Route path="/document" element={<MyDocuments/>} />
-//         <Route path="/rides" element={<MyRides/>} />
-//         <Route path="/vehicles" element={<Vehicles/>} />
-//         <Route path="/riderinvoice" element={<RiderInvoices/>} />
-//         <Route path="/balance" element={<BalanceReports/>} />
-//         <Route path="/tax" element={<TaxReports/>} />
-//         <Route path="/guide" element={<GuidesFAQ/>} />
-
-
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default App;
--- go-taxi-driver.web/src/pages/Profile.js
+++ go-taxi-driver.web/src/pages/Profile.js
@@ -33,15 +33,18 @@ const Profile = () => {
   };
 
   return (
-    <Paper style={{ padding: 16, margin: '16px 0' }}>
+    <Paper elevation={0} style={{ padding: 16, margin: '16px 0' }}>
+      <div className="bg-white p-30 min-w-md w-full mx-auto">
+
      
-      <h1 className="mt-2 text-3xl font-bold text-gray-900">My Profile</h1>
-      <h5 className="mt-2 text-3xl font-bold text-gray-900"></h5>
-      <h5 className="mt-2 text-3xl font-bold text-gray-900"></h5>
+      <h1 className="mt-2 text-3xl font-semibold text-gray-900">My Profile</h1>
+      
       <div className="text-left">
       <p className="font-medium text-primary-600 hover:text-primary-500">Manage your personal information</p>  
           </div>
       <form className="space-y-10" onSubmit={handleSaveProfile}>
+      <div>
+      <label htmlFor="name" className="block text-sm font-semibold text-gray-700">Name</label>
       <input 
               type="name"
               name="name"
@@ -51,6 +54,9 @@ const Profile = () => {
               className="w-full p-3 border border-gray-300 rounded-md"
               required
             />
+            </div>
+            <div>
+            <label htmlFor="email" className="block text-sm font-semibold text-gray-700">Email</label>
         <input
               type="email"
               name="email"
@@ -60,6 +66,9 @@ const Profile = () => {
               className="w-full p-3 border border-gray-300 rounded-md"
               required
             />
+            </div>
+            <div>
+            <label htmlFor="phonenumber" className="block text-sm font-semibold text-gray-700">Phonenumber</label>
         <input
               type="phoneNumber"
               name="phonenumber"
@@ -69,6 +78,9 @@ const Profile = () => {
               className="w-full p-3 border border-gray-300 rounded-md"
               required
             />
+            </div>
+            <div>
+            <label htmlFor="licensenumber" className="block text-sm font-semibold text-gray-700">Licensenumber</label>
         <input
               type="licenseNumber"
               name="licensenumber"
@@ -78,11 +90,12 @@ const Profile = () => {
               className="w-full p-3 border border-gray-300 rounded-md"
               required
             />
+            </div>
         {editMode ? (
   <button
     type="button"
     className="w-min flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium"
-    style={{ backgroundColor: '#b7a7a7', color: themeColors.text }}
+    style={{ backgroundColor: '#bab7b7', color: themeColors.text }}
     onClick={handleSaveProfile}
   >
     Spara
@@ -91,7 +104,7 @@ const Profile = () => {
   <button
     type="button"
     className="w-min flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium"
-    style={{ backgroundColor: '#b7a7a7', color: themeColors.text }}
+    style={{ backgroundColor: '#bab7b7', color: themeColors.text }}
     onClick={toggleEditMode}
   >
     Redigera
@@ -99,7 +112,9 @@ const Profile = () => {
 )}
 
       </form>
+      </div>
     </Paper>
+
   );
 };
 
