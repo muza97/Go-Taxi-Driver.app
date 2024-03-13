5fb2ae42e6b0567800fcaadced058e5283be8657
danielkarimi <daniel_karimii@outlook.com>
2024-03-14 00:53:52 +0100

    changes

--- go-taxi-driver.web/src/pages/LandingPage.js
+++ go-taxi-driver.web/src/pages/LandingPage.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import "./LandingPage.css"
 
 const Landing = () => {
    const token = localStorage.getItem('driverToken');
+   return (
+
+    <div className='Container'>
+
+    <div className="firstContainer">
+        <div className='StaticText'> <h1>DASHBOARD</h1>
+        <p>Welcome to your dashboard</p></div>
+    </div>
+
+    <div className="secondContainer">
+        <div className='Earnings'>
+            <p>Earning overview</p>
+
+        </div>
 
 
-    // return (
-    //     <div>
-    //         <div>Landing</div>
-    //         <LogoutButton /> {/* Använd LogoutButton-komponenten här */}
-    //     </div>
-    // );
+        <div className='WorkHours'>
+            <p>Working overview</p>
+
+        </div>
+    </div>
+
+</div>
+
+
+
+
+   );
 };
 
 export default Landing;
